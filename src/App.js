a8d946b38a3fc7ed37f02932ea91567f2413e77b
TobiasSvenssonDev <tobias_svensson84@live.se>
2022-05-19 00:39:51 +0200

    API setup
    Nasa APOD API konfigurerat svar:200 ok

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import NasaApodAPI from "./NasaApodAPI";
+
 function App() {
+
   return (
     <div className="container">
       <h1>Space</h1>
+      <NasaApodAPI />
     </div>
   );
 }
--- src/NasaApodAPI.js
+++ src/NasaApodAPI.js
@@ -1,7 +1,36 @@
 import React from 'react'
+import axios from 'axios';
 
 export default function NasaApodAPI() {
+  //const [apods, setApod] = useState(getApod)
+ 
+
+  async function getApod() {
+
+    const url = 'https://api.nasa.gov/planetary/apod';
+    const apiKey = 'A3sumYKTb72gWZ2K9RcwfMo73LRjtIJy9fO579RE';
+
+    await axios.get(url, {
+      params: {
+        'api_key': apiKey,  
+      }
+    })
+      .then(function (response) {
+        console.log(response.data.hdurl + " SUCCESS ");
+        return response.data
+        
+      })
+      .catch(function (error) {
+        console.log(error + " FEL");
+      });
+  }
+
   return (
-    <div>NasaApodAPI</div>
+    <div><h1>APOD API</h1>
+      <div>
+        <button className="btn btn-lg btn-success" onClick={getApod}>Get started</button>
+      </div>
+
+    </div>
   )
 }
