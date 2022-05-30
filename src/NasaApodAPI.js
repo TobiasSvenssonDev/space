ee7c51a283edfc64d574b8f094605516f17e3fca
TobiasSvenssonDev <tobias_svensson84@live.se>
2022-05-30 21:09:29 +0200

    fixade npm warning om felaktig depency i react-hooks

--- src/NasaApodAPI.js
+++ src/NasaApodAPI.js
@@ -6,34 +6,35 @@ const baseURL = 'https://api.nasa.gov/planetary/apod'
 
 export default function NasaApodAPI() {
 
-    const [apods, setApods] = useState(null);
-    console.log(apods);
+  const [apods, setApods] = useState(null);
+  console.log(apods);
+  
+  useEffect(() => {
     const apiKey = 'A3sumYKTb72gWZ2K9RcwfMo73LRjtIJy9fO579RE';
     const currentDate = new Date();
 
     currentDate.setDate(currentDate.getDate() - 7);
 
     const lastWeek = `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`;
-    console.log(lastWeek)
 
-    useEffect(() => {
-        axios.get(baseURL, {
-            params: {
-              'api_key': apiKey,
-              'start_date': lastWeek
-            }}).then((response) => {
-            console.log(response.data);
-            setApods(response.data);
-        });
-    }, []);
+    axios.get(baseURL, {
+      params: {
+        'api_key': apiKey,
+        'start_date': lastWeek
+      }
+    }).then((response) => {
+      console.log(response.data);
+      setApods(response.data);
+    });
+  }, []);
 
-    if (!apods) return null;
-// {movies.map(movie => <Movie key={movie.id} item={movie} deleteMovie={deleteMovie} />)}
+  if (!apods) return null;
+  // {movies.map(movie => <Movie key={movie.id} item={movie} deleteMovie={deleteMovie} />)}
   return (
     <div>NasaApodAPI
-        <div>
-            {apods.map(apod => <Apod key={apod.date} item={apod} />)}
-        </div>
+      <div>
+        {apods.map(apod => <Apod key={apod.date} item={apod} />)}
+      </div>
     </div>
   )
 }
