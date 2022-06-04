68dcc1cb66cb43f51756748aa1d95a51f0dd58d8
TobiasSvenssonDev <tobias_svensson84@live.se>
2022-06-04 23:30:06 +0200

    mer css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import NasaApodAPI from "./NasaApodAPI";
 import 'react-tippy/dist/tippy.css';
 
-
 function App() {
 
-  return (
-    <div className="container">
-      <header className="header">
-        <h1 id="brand">NASA APOD</h1>
-      </header>
-      <main className="main">
-        <NasaApodAPI />
-      </main>
-      <footer className="footer">
-        SIDFOT
-      </footer>
-    </div>
+  return (<>
+    <header className="header">
+      <h1 id="brand">NASA APOD</h1>
+    </header>
+    <main className="main">
+      <NasaApodAPI />
+    </main>
+    <footer className="footer">
+      SIDFOT
+    </footer>
+  </>
   );
 }
 
